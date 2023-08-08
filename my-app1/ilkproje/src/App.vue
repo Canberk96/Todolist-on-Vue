5369a99586e2d96da7c03d9481f4a28edf65255d
Canberk96 <canberkarman@gmail.com>
2023-08-08 17:14:20 +0300

    todo list on Vue.js

---
+++ my-app1/ilkproje/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <h3>Todo List</h3>
+  <ul>
+    <todo-item 
+    v-for="x in items"
+    :key="x"
+    :item-name="x"
+    style="background-color: lightgreen;"
+    />
+  </ul>
+  <input v-model="newItem">
+  <button @click="addItem">Add</button>
+  <button @click="removeItem">Remove</button>
+  
+
+  
+  
+</template>
+
+<script>
+export default{
+  methods : {
+    addItem(){
+      this.items.push(this.newItem);
+      this.newItem = '';
+    },
+    removeItem(){
+      this.items.pop(this.newItem);
+      this.newItem = '';
+    }
+  },
+
+  data(){
+
+    return{
+      newItem: '',
+      items: ['Buy apples','Make pizza','Mow the lawn']
+        
+        };
+      
+    }
+  }
+
+
+
+
+
+
+</script>
+
+<style>
+#wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
+    border: dashed black 1px;
+    margin: 10px;
+    padding: 0 20px;
+    background-color: lightgreen;
+  }
+
+
+
+</style>
---
+++ my-app1/ilkproje/src/components/FoodItem.vue
@@ -0,0 +1,58 @@
+
+<template>
+    <div>
+      <h2>{{ foodName }}
+    <img src="/55364608-meat-kebab.jpg" v-show="isFavorite"> 
+    
+    </h2>
+    <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
+    
+      
+      
+      
+      
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    
+    
+
+  props:['foodName','foodDesc','isFavorite'],
+  emits:['toggleFavorite'],
+  data(){
+    return{
+       // foodIsFavorite: this.isFavorite
+    }
+  },
+  methods: {
+    
+    toggleFavorite(){
+       // this.foodIsFavorite =!this.foodIsFavorite
+        this.$emit('toggle-favorite',this.foodName);
+
+    }
+    
+  }
+       
+       
+    }
+    
+       
+    
+    
+    
+
+  
+  </script>
+  
+  <style>
+img{
+    height: 100px;
+    float: right;
+}
+
+</style>
